5b363f2782658e578f7ee0d0482b1718daffdcc5
guoyangyang1994 <a20131105798@admindeiMac-12.local>
2016-04-11 20:46:03 +0800

    按钮

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jsq
+//
+//  Created by 20131105798 on 16/3/28.
+//  Copyright © 2016年 20131105798. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
